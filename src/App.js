61ac49a689c03ac237dcdc488a6de82420a025ad
Amir Hosen <pulchra.1130@gmail.com>
2018-04-20 15:46:46 +0800

    Adds prettier and huksy for pre commit hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import 'corgicss/css/corgi.css'
+import React, { Component } from "react";
+import "corgicss/css/corgi.css";
 
 class App extends Component {
   render() {
