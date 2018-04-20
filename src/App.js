ce9001788cfd32be90dac8d124eacd546c3b6865
Amir Hosen <pulchra.1130@gmail.com>
2018-04-20 15:53:45 +0800

    Login form html

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React, { Component } from "react";
 import "corgicss/css/corgi.css";
 
 class App extends Component {
+  submit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   render() {
     return (
       <div className="container">
-        <h1>Hello world</h1>
+        <form className="form" onSubmit={this.submit}>
+          <fieldset>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+
+            <button className="button -primary" type="submit">
+              Log in
+            </button>
+          </fieldset>
+        </form>
       </div>
     );
   }
