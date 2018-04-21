d1b98333240ebea5a83060270719f3984e32f635
Amir Hosen <pulchra.1130@gmail.com>
2018-04-21 13:58:28 +0800

    adds react-router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./containers/Login/";
 import "corgicss/css/corgi.css";
 
 class App extends Component {
-  submit(e) {
-    e.preventDefault();
-    console.log(e);
-  }
-
   render() {
     return (
-      <div className="container">
-        <form className="form" onSubmit={this.submit}>
-          <fieldset>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" />
-
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-
-            <button className="button -primary" type="submit">
-              Log in
-            </button>
-          </fieldset>
-        </form>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/containers/Login/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class Login extends Component {
+  submit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <form className="form" onSubmit={this.submit}>
+          <fieldset>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" />
+
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" />
+
+            <button className="button -primary" type="submit">
+              Log in
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
