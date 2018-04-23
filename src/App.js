e20548d768adfde0b3df2e8b4e18ca582a93b8ed
Amir Hosen <pulchra.1130@gmail.com>
2018-04-23 20:23:50 +0800

    adds navbar, wuhoo!

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./containers/Login";
 import Landing from "./containers/Landing";
+import Navbar from "./containers/Navbar";
 import "corgicss/css/corgi.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <Route exact path="/" component={Landing} />
-          <Route path="/login" component={Login} />
-        </div>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Route exact path="/" component={Landing} />
+            <Route path="/login" component={Login} />
+          </div>
+        </React.Fragment>
       </Router>
     );
   }
---
+++ src/containers/Navbar/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
