85c15a5ce552510261d4cf72b644e496905d0a66
Amir Hosen <pulchra.1130@gmail.com>
2018-04-29 13:27:29 +0800

    tries out formik!

---
+++ src/containers/Login/Form.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { withFormik } from "formik";
+
+const InnerForm = ({
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting
+}) => (
+  <form className="form" onSubmit={handleSubmit}>
+    <fieldset>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        name="username"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.username}
+      />
+      {touched.username && errors.username && <div>{errors.username}</div>}
+
+      <label htmlFor="username">Password</label>
+      <input
+        type="text"
+        name="password"
+        onChange={handleChange}
+        onBlur={handleBlur}
+        value={values.password}
+      />
+      {touched.password && errors.password && <div>{errors.password}</div>}
+
+      <button className="button -primary" type="submit" disabled={isSubmitting}>
+        Log in
+      </button>
+    </fieldset>
+  </form>
+);
+
+export default props => {
+  const Enhanced = withFormik({
+    mapPropsToValues: props => ({ username: "", password: "" }),
+    validate: (values, props) => {
+      const errors = {};
+      return errors;
+    },
+    handleSubmit: props.handleSubmit
+  })(InnerForm);
+
+  return <Enhanced />;
+};
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -2,33 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { authRequest } from "../../actions/auth";
 
-class Login extends Component {
-  submit(e) {
-    e.preventDefault();
+import Form from "./Form";
 
-    // probably need to add formik at some point
-    this.props.authRequest({
-      email: "email",
-      password: "password"
-    });
-  }
+class Login extends Component {
+  submit = (values, { props, setSubmitting, setErrors }) => {
+    this.props.authRequest(values);
+  };
 
   render() {
     return (
       <div className="container">
-        <form className="form" onSubmit={this.submit.bind(this)}>
-          <fieldset>
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" />
-
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
-
-            <button className="button -primary" type="submit">
-              Log in
-            </button>
-          </fieldset>
-        </form>
+        <Form handleSubmit={this.submit} />
       </div>
     );
   }
