c0e91008501b23eeedc1411fd379d354dd359a60
Amir Hosen <pulchra.1130@gmail.com>
2019-03-09 20:35:24 +0800

    housekeeping part 2

--- src/store.js
+++ src/store.js
@@ -1,22 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import createSagaMiddleware from "redux-saga";
-import { all } from "redux-saga/effects";
-
-import authSaga from "./containers/Login/saga";
-
-const rootSaga = function*() {
-  yield all([authSaga()]);
-};
-
-const sagaMiddleware = createSagaMiddleware(...rootSaga);
 
 const reducer = (state = {}, action) => {
   return state;
 };
 
-const store = createStore(reducer, applyMiddleware(logger, sagaMiddleware));
-
-sagaMiddleware.run(rootSaga);
-
+const store = createStore(reducer, applyMiddleware(logger));
 export default store;
